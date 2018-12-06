8f5adbb76c6f93ce9ed24975ad7d867500dfb4c5
kimseowoo <swoo422@naver.com>
2018-12-06 15:57:40 +0900

    5

--- linkedList.c
+++ linkedList.c
@@ -11,8 +11,8 @@ typedef struct linknd{
 
 static linknd_t *list; 	//linked list 실체
 
-linknd_t* create_node(int value){
-	linknd_t* ndPtr;
+linknd_t *create_node(int value){
+	linknd_t *ndPtr;
 	
 	//동적 메모리 할당
 	ndPtr = (linknd_t*)malloc( sizeof(linknd_t) );
@@ -27,4 +27,26 @@ linknd_t* create_node(int value){
 	ndPtr->next = NULL;   	// 핵심 꼮 해야함 
 	
 	return ndPtr;
+}
+
+
+
+void addTail(int value){
+	linknd_t *ndPtr, *newPtr;
+	
+	if(list == NULL)
+	{
+		return;
+	}
+	else
+	{
+		ndPtr = list;
+		while (ndPtr->next != NULL){
+			ndPtr = ndPtr-> next;
+		}
+		
+		newPtr = create_node(value);
+		ndPtr->next = newPtr;
+	}
+	
 }
